13b6a45fac74127eac8bf712383f9b0509bba5ab
Noraa <noha0002@stud.kea.dk>
2025-03-11 19:53:32 +0100

    javascript

---
+++ java.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+ 
+  document.getElementById("menubtn").addEventListener("click", function () {
+    document.getElementById("sidenav").classList.toggle("active");
+  });
+});
