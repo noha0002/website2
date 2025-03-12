f373227fcb1201d8bf7114af6487551716474b96
Noraa <noha0002@stud.kea.dk>
2025-03-12 11:10:11 +0100

    kommentar

--- java.js
+++ java.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
- 
   document.getElementById("menubtn").addEventListener("click", function () {
     document.getElementById("sidenav").classList.toggle("active");
   });
